68a4054ca856ace9427101cee1438167c42187ff
David Holland <dholland>
2017-05-04 19:42:01 -0400

    prune old stuff.

--- src/astprops.v
+++ src/astprops.v
@@ -154,62 +154,6 @@ with
      VarsUniqueProc pt rt (proc pt rt x body)
 .
 
-(*
-(* check that variable declarations are unique *)
-Inductive StmtDeclaresVar: forall t, Stmt -> Var t -> bool -> Prop :=
-| block_declares_var_nil: forall t v,
-     StmtDeclaresVar t (block []) v false
-| block_declares_var_cons_here: forall t s ss v,
-     StmtDeclaresVar t s v true -> StmtDeclaresVar t (block ss) v false ->
-     StmtDeclaresVar t (block (s :: ss)) v true
-| block_declares_var_cons_nothere: forall t s ss v b,
-     StmtDeclaresVar t s v false -> StmtDeclaresVar t (block ss) v b ->
-     StmtDeclaresVar t (block (s :: ss)) v b
-| start_declares_var: forall t s v b pt p e,
-     StmtDeclaresVar t s v b ->
-     StmtDeclaresVar t (start pt p e) v false
-| assign_declares_var: forall t' t e e' v',
-     StmtDeclaresVar t' (assign t e e') v' false
-| load_declares_var: forall t' t v e v',
-     StmtDeclaresVar t' (load t v e) v' false
-| store_declares_var: forall t' t v e v',
-     StmtDeclaresVar t' (store t v e) v' false
-| if_declares_var: forall t e s1 s2 v b1 b2,
-     StmtDeclaresVar t s1 v b1 -> StmtDeclaresVar t s2 v b2 ->
-     StmtDeclaresVar t (if_ e s1 s2) v (b1 || b2)
-| while_declares_var: forall t e s v b,
-     StmtDeclaresVar t s v b ->
-     StmtDeclaresVar t (while e s) v b
-| call_declares_var: forall t' (pt rt : Set) v p e v' b,
-     ProcDeclaresVar t' pt rt p v' b ->
-     StmtDeclaresVar t' (call pt rt v p e) v' b
-| local_declares_var: forall t v e,
-     StmtDeclaresVar t (local t v e) v true
-| return_declares_var: forall t' t e v',
-     StmtDeclaresVar t' (return_ t e) v' false
-with
-(*Inductive*) ProcDeclaresVar: forall t pt rt, Proc pt rt -> Var t -> bool -> Prop :=
-| proc_declares_var_arg: forall pt rt s v,
-     StmtDeclaresVar pt s v false ->
-     ProcDeclaresVar pt pt rt (proc pt rt v s) v true
-| proc_declares_var_nonarg_sametype: forall pt rt s v' v b,
-     StmtDeclaresVar pt s v b ->
-     v <> v' ->
-     ProcDeclaresVar pt pt rt (proc pt rt v' s) v b
-| proc_declares_var_nonarg_othertype: forall t pt rt s v' v b,
-     StmtDeclaresVar t s v b ->
-     t <> pt ->
-     ProcDeclaresVar t pt rt (proc pt rt v' s) v b
-.
-
-(* check that variable uses are after declarations *)
-(*
-Inductive StmtLooseVars: forall t, Stmt -> list (Var t) -> Prop :=
-| block_loose_vars_nil: forall t,
-     StmtLooseVars t (block []) []
-| (* TBD *)
-*)
-*)
 
 (* check that procedure returns are ok *)
 

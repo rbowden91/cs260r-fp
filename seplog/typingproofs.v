7421dba1c0a3ddadd1d6ed745adc6839ba3ffc27
David Holland <dholland>
2017-05-08 09:25:25 -0400

    bunch more typing stuff, plus fix after new expressions added

--- seplog/typingproofs.v
+++ seplog/typingproofs.v
@@ -139,6 +139,9 @@ Proof.
    (*remember e as e1. revert Heqe1. revert e1.*)
    induction e; intros.
    - subst. exists v. inversion H; subst. apply value_yields.
+   - inversion H; subst.
+     (* this got added at the last minute and I'm not sure how it's supposed to work *)
+     admit.
    - subst. inversion H; subst.
      unfold localenv_sound in H0.
      apply H0 in H4.
@@ -182,15 +185,30 @@ Proof.
         [exists vt; apply cond_true_yields | exists vf; apply cond_false_yields];
         auto.
 *)
-   remember H6 as H6a. clear HeqH6a.
-   apply ExprYieldsPreserves with (tyenv := tyenv) in H6; auto.
-   destruct pred.
-   1-2,4-5,8: unfold type_of_value in H6; discriminate.
-   2,3: unfold type_of_value in H6; destruct a; discriminate.
-   destruct b.
-   * exists vt. apply cond_true_yields; auto.
-   * exists vf. apply cond_false_yields; auto.
-Qed.
+     remember H6 as H6a. clear HeqH6a.
+     apply ExprYieldsPreserves with (tyenv := tyenv) in H6; auto.
+     destruct pred.
+     1-2,4-5,8: unfold type_of_value in H6; discriminate.
+     2,3: unfold type_of_value in H6; destruct a; discriminate.
+     destruct b.
+     * exists vt. apply cond_true_yields; auto.
+     * exists vf. apply cond_false_yields; auto.
+  - inversion H; subst.
+    apply IHe1 with (l := l) in H5; auto.
+    apply IHe2 with (l := l) in H7; auto.
+    destruct H5 as [a1 H5].
+    destruct H7 as [a2 H7].
+    inversion H; subst.
+    assert (type_of_value a1 = t_nat) by
+       (apply ExprYieldsPreserves with (tyenv := tyenv) in H5; auto).
+    assert (type_of_value a2 = t_nat) by
+       (apply ExprYieldsPreserves with (tyenv := tyenv) in H7; auto).
+    destruct a1; simpl in H1; try discriminate.
+    destruct a2; simpl in H2; try discriminate.
+    exists (v_nat (n n0 n1)).
+    apply natbinop_yields; auto.
+    all: destruct a; discriminate.
+Admitted.
 
 (**************************************************************)
 (* statements *)
@@ -473,32 +491,314 @@ Admitted.
 (**************************************************************)
 (* stacks *)
 
-Definition StackFrameSound tyenv stk :=
-   match stk with
-   | stack_empty => False
-   | stack_frame loc stk' s =>
-        StmtTyped tyenv s /\ localenv_sound tyenv loc
-   end.
+Inductive StackSound tyenv: Stack -> Prop :=
+| stack_sound_base:
+     forall loc s,
+     StmtTyped tyenv s -> localenv_sound tyenv loc ->
+     StackSound tyenv (stack_frame loc stack_empty s)
+| stack_sound_inner:
+     forall loc s stk',
+     StackSound tyenv stk' ->
+     StmtTyped tyenv s -> localenv_sound tyenv loc ->
+     StackSound tyenv (stack_frame loc stk' s)
+.
 
 (*
  * preservation for stacks
  *)
 
+Lemma StackStepsPreserves:
+   forall tyenv h stk,
+   StackSound tyenv stk ->
+   forall h' stk',
+      StackSteps h stk h' stk' ->
+      StackSound tyenv stk'.
+Proof.
+   intros.
+   induction H0.
+   - (* statement level cases *)
+     inversion H; subst; [apply stack_sound_base | apply stack_sound_inner];
+        apply StmtStepsPreserves with (tyenv := tyenv) in H0; auto.
+     (* rolling this into the previous doesn't work tidily *)
+     all: destruct H0; auto.
+   - (* tail call *)
+     inversion H; subst; [apply stack_sound_base | apply stack_sound_inner]; auto.
+     all: apply s_seq_typed; auto; apply s_skip_typed.
+   - (* general call *)
+     apply stack_sound_inner.
+     * (* show that the updated caller frame is sound *)
+       inversion H; subst; [apply stack_sound_base | apply stack_sound_inner]; auto.
+       + inversion H6; subst.
+         apply s_seq_typed; auto.
+         apply s_assign_typed.
+         -- (* note: v_undef should have type forall t, t; it's actually t_unit *)
+            (* (that's not good enough here) *)
+            apply e_value_typed; simpl.
+            admit.
+         -- inversion H5; subst; auto.
+       + inversion H8; subst.
+         apply s_seq_typed; auto.
+         apply s_assign_typed.
+         -- (* ditto *)
+            apply e_value_typed; simpl.
+            admit.
+         -- inversion H5; subst; auto.
+     * (* show that the callee's body is well typed *)
+       (* XXX except that it's supposed to be wrt the caller's type environment. bogus *)
+(*
+       inversion H; subst.
+       inversion H6; subst.
+       inversion H5; subst.
+       inversion H10; subst.
+       auto.
+*)
+       admit.
+     * (* show that the new local environment is sound *)
+       (* this requires VardeclsStepsPreserves *)
+       admit.
+    - (* return followed by crap *)
+      inversion H; subst; [apply stack_sound_base | apply stack_sound_inner]; auto.
+      * inversion H2; subst; auto.
+      * inversion H4; subst; auto.
+    - (* general return *)
+      inversion H; subst.
+      inversion H6; subst;
+         apply stack_sound_inner; auto.
+      * (*
+         * this wants us to prove that an empty stack is sound, which it isn't;
+         * this is because we don't have anything that prohibits that, and more
+         * specifically, we don't have a way to know that the initial frame
+         * consists of a call followed by skip such that it ultimately steps to
+         * StackDone.
+         *)
+        admit.
+      * (* Show that the residual assignment of the return value is typed. *)
+        destruct x.
+        (* currently we don't enforce return types properly *)
+        assert (ExprTyped rt tyenv ret) by admit.
+        assert (t = rt) by admit; subst.
+        apply s_assign_typed.
+        + apply e_value_typed.
+          apply ExprYieldsPreserves with (tyenv := tyenv) in H0; auto.
+        + inversion H3; subst; auto.
+      * (* carbon copy of previous case for different stack configuration *)
+        destruct x.
+        (* currently we don't enforce return types properly *)
+        assert (ExprTyped rt tyenv ret) by admit.
+        assert (t = rt) by admit; subst.
+        apply s_assign_typed.
+        + apply e_value_typed.
+          apply ExprYieldsPreserves with (tyenv := tyenv) in H0; auto.
+        + inversion H5; subst; auto.
+Admitted.
+
+Lemma StackStepsStartPreserves:
+   forall tyenv stk,
+   StackSound tyenv stk ->
+   forall stk' stk2,
+      StackStepsStart stk stk' stk2 ->
+      exists tyenv2,
+         StackSound tyenv stk' /\
+         StackSound tyenv2 stk2.
+Proof.
+   (* not yet... *)
+Admitted.
 
 (*
  * progress for stacks
  *)
 
+Lemma StackStepsProgress_seq_sublemma:
+   forall tyenv h loc stk0 s1 s2,
+   StmtTyped tyenv s1 ->
+   localenv_sound tyenv loc ->
+   (exists h' stk', StackSteps h (stack_frame loc stk0 s1) h' stk') ->
+   exists h' stk',
+      StackSteps h (stack_frame loc stk0 (s_seq s1 s2)) h' stk'.
+Proof.
+   intros.
+   destruct H1 as [h' [stk' H1]].
+   destruct stk'; inversion H1; subst.
+   - (* stmt case *)
+     exists h', (stack_frame l stk' (s_seq s s2)).
+     apply stack_steps_stmt.
+     apply step_in_seq.
+     auto.
+   - (* tail call *)
+     (* XXX do this *)
+     admit.
+   - (* full call *)
+     (* XXX do this *)
+     admit.
+   - (* return followed by crap (inside a seq) *)
+     (* XXX this case doesn't work *) 
+     admit.
+   - (* return *)
+     exists h', (stack_frame loc (stack_frame l stk' (s_assign x ejunk)) (s_return ret)).
+     apply stack_steps_return_seq.
+Admitted.
+
+Lemma StackStepsProgress:
+   forall tyenv h stk,
+   StackSound tyenv stk ->
+   ~(StackDone stk) ->
+   (exists h' stk',
+      StackSteps h stk h' stk') \/
+   (exists stk' stk2,
+      StackStepsStart stk stk' stk2).
+Proof.
+   intros.
+   (* this has too damn many cases, need to figure out how to clean it up *)
+   destruct H; [revert H H0 H1 | revert H H0 H1 H2].
+   all: revert loc h tyenv.
+   all: induction s; intros.
+   (* bottom of stack cases *)
+   - (* skip *)
+     contradict H0. apply stack_done.
+   - (* seq *)
+     inversion H; subst.
+     destruct s1.
+     (* get the skip case, which is different *)
+     Focus 1.
+        left.
+        exists h, (stack_frame loc stack_empty s2); apply stack_steps_stmt; apply step_next.
+     (* get the start case, which is different *)
+     Focus 2.
+        apply IHs1 with (h := h) in H1; auto; [ | contradict H0; inversion H0 ].
+        destruct H1.
+        destruct H1 as [h' [stk' H1]]; inversion H1; inversion H10; subst; discriminate.
+        destruct H1 as [stk' [stk2 H1]]. right; exists stk', stk2.
+        (* XXX this case doesn't work (start inside a seq) *)
+        admit.
+     (* the rest of the cases are the same *)
+     all: left.
+     all: apply StackStepsProgress_seq_sublemma with (tyenv := tyenv); auto.
+     all: apply IHs1 with (h := h) in H1; auto.
+     2,4,6,8,10,12,14,16,18,20: contradict H0; inversion H0.
+     all: destruct H1; auto; destruct H1 as [stk' [stk2 H1]]; inversion H1; subst.
+   - (* start *)
+     right; inversion H; subst.
+     (* haven't done this yet *)
+     admit.
+   - (* assign *)
+     left; inversion H; subst.
+     apply StmtStepsProgress_assign with (h := h) (l := loc) in H; auto.
+     destruct H as [h' [l' H]].
+     exists h', (stack_frame l' stack_empty s_skip).
+     apply stack_steps_stmt; auto.
+   - (* load *)
+     left; inversion H; subst.
+     apply StmtStepsProgress_load with (h := h) (l := loc) in H; auto.
+     destruct H as [h' [l' H]].
+     exists h', (stack_frame l' stack_empty s_skip).
+     apply stack_steps_stmt; auto.
+   - (* store *)
+     left; inversion H; subst.
+     apply StmtStepsProgress_store with (h := h) (l := loc) in H; auto.
+     destruct H as [h' [l' H]].
+     exists h', (stack_frame l' stack_empty s_skip).
+     apply stack_steps_stmt; auto.
+   - (* if *)
+     left; inversion H; subst.
+     apply StmtStepsProgress_if with (h := h) (l := loc) in H; auto.
+     destruct H as [h' [l' [s' H]]].
+     exists h', (stack_frame l' stack_empty s').
+     apply stack_steps_stmt; auto.
+   - (* while *)
+     left; inversion H; subst.
+     apply StmtStepsProgress_while with (h := h) (l := loc) in H; auto.
+     destruct H as [h' [l' [s' H]]].
+     exists h', (stack_frame l' stack_empty s').
+     apply stack_steps_stmt; auto.
+   - (* call *)
+     left; inversion H; subst.
+     (* haven't done this yet *)
+     admit.
+   - (* return *)
+     left; inversion H; subst.
+     (* this case is illegal but we don't know how to rule it out yet (see above) *)
+     admit.
+   - (* getlock *)
+     left; inversion H; subst.
+     apply StmtStepsProgress_getlock with (h := h) (l := loc) in H; auto.
+     destruct H as [h' [l' H]].
+     exists h', (stack_frame l' stack_empty s_skip).
+     apply stack_steps_stmt; auto.
+   - (* putlock *)
+     left; inversion H; subst.
+     apply StmtStepsProgress_putlock with (h := h) (l := loc) in H; auto.
+     destruct H as [h' [l' H]].
+     exists h', (stack_frame l' stack_empty s_skip).
+     apply stack_steps_stmt; auto.
+   (* now come the mostly duplicate not-bottom-of-stack cases *)
+   - (* skip at end of procedure missing its return *)
+     (* illegal but not yet ruled out *)
+     admit.
+   - (* seq *)
+     (* this is ultimately identical to the mess above *)
+     admit.
+   - (* start *)
+     right; inversion H1; subst.
+     (* haven't done this yet *)
+     admit.
+   - (* assign *)
+     left; inversion H1; subst.
+     apply StmtStepsProgress_assign with (h := h) (l := loc) in H1; auto.
+     destruct H1 as [h' [l' H1]].
+     exists h', (stack_frame l' stk' s_skip).
+     apply stack_steps_stmt; auto.
+   - (* load *)
+     left; inversion H1; subst.
+     apply StmtStepsProgress_load with (h := h) (l := loc) in H1; auto.
+     destruct H1 as [h' [l' H1]].
+     exists h', (stack_frame l' stk' s_skip).
+     apply stack_steps_stmt; auto.
+   - (* store *)
+     left; inversion H1; subst.
+     apply StmtStepsProgress_store with (h := h) (l := loc) in H1; auto.
+     destruct H1 as [h' [l' H1]].
+     exists h', (stack_frame l' stk' s_skip).
+     apply stack_steps_stmt; auto.
+   - (* if *)
+     left; inversion H1; subst.
+     apply StmtStepsProgress_if with (h := h) (l := loc) in H1; auto.
+     destruct H1 as [h' [l' [s' H1]]].
+     exists h', (stack_frame l' stk' s').
+     apply stack_steps_stmt; auto.
+   - (* while *)
+     left; inversion H1; subst.
+     apply StmtStepsProgress_while with (h := h) (l := loc) in H1; auto.
+     destruct H1 as [h' [l' [s' H1]]].
+     exists h', (stack_frame l' stk' s').
+     apply stack_steps_stmt; auto.
+   - (* call *)
+     left; inversion H1; subst.
+     (* haven't done this yet *)
+     admit.
+   - (* return *)
+     left; inversion H1; subst.
+     (* haven't done this yet *)
+     admit.
+   - (* getlock *)
+     left; inversion H1; subst.
+     apply StmtStepsProgress_getlock with (h := h) (l := loc) in H1; auto.
+     destruct H1 as [h' [l' H1]].
+     exists h', (stack_frame l' stk' s_skip).
+     apply stack_steps_stmt; auto.
+   - (* putlock *)
+     left; inversion H1; subst.
+     apply StmtStepsProgress_putlock with (h := h) (l := loc) in H1; auto.
+     destruct H1 as [h' [l' H1]].
+     exists h', (stack_frame l' stk' s_skip).
+     apply stack_steps_stmt; auto.
+Admitted.
+
 (**************************************************************)
 (* threads *)
 
-Definition ThreadStateSound tyenv t :=
+Definition ThreadSound tyenv t :=
    match t with
-   | thread (stack_empty) => False
-   | thread (stack_frame loc stk s) =>
-        StmtTyped tyenv s /\
-        localenv_sound tyenv loc (* /\
-        stack_sound tyenv stk *)
+   | thread stk => StackSound tyenv stk
    end.
 
 (*
@@ -507,46 +807,35 @@ Definition ThreadStateSound tyenv t :=
 
 Lemma ThreadStepsPreserves:
    forall tyenv h t,
-      ThreadStateSound tyenv t ->
+      ThreadSound tyenv t ->
       forall h' t',
          ThreadSteps h t h' t' ->
-         ThreadStateSound tyenv t'.
+         ThreadSound tyenv t'.
 Proof.
+   unfold ThreadSound.
+   destruct t.
+   destruct t'.
    intros.
-   destruct H0.
-   induction H0.
-   - unfold ThreadStateSound in *.
-     destruct H as [H1 H2].
-     apply StmtStepsPreserves with
-         (tyenv := tyenv)
-            (h := h) (l := loc) (s := s)
-            (h' := h') (l' := loc') (s' := s') in H0; auto.
-   - (* call *)
-     unfold ThreadStateSound in *.
-     destruct H as [H1 H2].
-     admit.
-   - (* call #2 *)
-     unfold ThreadStateSound in *.
-     destruct H as [Ha Hb].
-     admit.
-   - (* return *)
-     admit.
-   - (* return #2 *)
-     admit.
-Admitted.
+   inversion H0; subst.
+   apply StackStepsPreserves with (h := h) (stk := s) (h' := h'); auto.
+Qed.
 
 Lemma ThreadStepsStartPreserves:
    forall tyenv t,
-      ThreadStateSound tyenv t ->
+      ThreadSound tyenv t ->
       forall t' tnew,
          ThreadStepsStart t t' tnew ->
          exists tyenvnew,
-            ThreadStateSound tyenv t' /\
-            ThreadStateSound tyenvnew tnew.
+            ThreadSound tyenv t' /\
+            ThreadSound tyenvnew tnew.
 Proof.
+   unfold ThreadSound.
+   destruct t.
+   destruct t'.
    intros.
-   admit.
-Admitted.
+   inversion H0; subst.
+   apply StackStepsStartPreserves with (tyenv := tyenv) in H3; auto.
+Qed.
 
 (*
  * progress for threads
@@ -560,41 +849,56 @@ Definition threadstmt_is (t: Thread) s0 :=
 
 Lemma ThreadStepsProgress:
    forall tyenv h t,
-      ThreadStateSound tyenv t ->
-      (forall p arg, threadstmt_is t (s_start p arg) = False) ->
-      exists h' t',
-         ThreadSteps h t h' t'.
+      ThreadSound tyenv t ->
+      ~(ThreadDone t) ->
+      (exists h' t',
+         ThreadSteps h t h' t') \/
+      (exists t' t2,
+         ThreadStepsStart t t' t2).
 Proof.
-Admitted.
-
-Lemma ThreadStepsStartProgress:
-   forall tyenv t,
-      ThreadStateSound tyenv t ->
-      (exists p arg, threadstmt_is t (s_start p arg)) ->
-      exists t' tnew,
-         ThreadStepsStart t t' tnew.
-Proof.
-Admitted.
+   unfold ThreadSound.
+   destruct t.
+   intros.
+   apply StackStepsProgress with (h := h) in H.
+   destruct H.
+   - left.
+     destruct H as [h' [stk' H]].
+     exists h', (thread stk').
+     apply thread_steps; auto.
+   - right.
+     destruct H as [stk' [stk2 H]].
+     exists (thread stk'), (thread stk2).
+     apply thread_steps_start; auto.
+   - contradict H0.
+     apply thread_done; auto.
+Qed.
 
 (**************************************************************)
 (* machine *)
 
-Definition MachineEnv := nat. (* XXX *)
-Definition MachineStateSound (menv: MachineEnv) (m: Machine) (menv2: MachineEnv) :=
-   True. (* XXX *)
+Definition MachineSound (m: Machine) :=
+   match m with
+   | machine h ts => 
+        (* XXX XXX XXX this is no good, each thread needs its own tyenv *)
+        forall tyenv t, In t ts -> ThreadSound tyenv t
+   end.
 
 (*
  * preservation for machines
  *)
 
 Lemma MachineStepsPreserves:
-   forall menv menv2 m,
-      MachineStateSound menv m menv2 ->
+   forall m,
+      MachineSound m ->
       forall m',
          MachineSteps m m' ->
-         exists menv',
-            MachineStateSound menv' m' menv2.
+         MachineSound m'.
 Proof.
+   intros.
+   inversion H0; subst.
+   - admit.
+   - admit.
+   - admit.
 Admitted.
 
 (*
@@ -602,8 +906,8 @@ Admitted.
  *)
 
 Lemma MachineStepsProgress:
-   forall menv menv2 m,
-      MachineStateSound menv m menv2 ->
+   forall m,
+      MachineSound m ->
       exists m',
          MachineSteps m m'.
 Proof.
@@ -613,19 +917,18 @@ Admitted.
 (* top level *)
 
 Theorem general_type_soundness_of_ast_and_semantics:
-   forall menv menv2 m,
-      MachineStateSound menv m menv2 ->
+   forall m,
+      MachineSound m ->
       (exists m',
          MachineSteps m m') /\
       (forall m',
          MachineSteps m m' ->
-         exists menv',
-            MachineStateSound menv' m' menv2).
+         MachineSound m').
 Proof.
    intros.
    split.
-   - apply MachineStepsProgress with (menv := menv) (menv2 := menv2).
+   - apply MachineStepsProgress.
      auto.
-   - apply MachineStepsPreserves with (menv := menv).
+   - apply MachineStepsPreserves.
      auto.
 Qed.
